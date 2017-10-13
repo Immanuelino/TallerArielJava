f0bde0c6a7fbf491bff6333347373065fc7fff58
personal <personal@192.168.141.1>
2017-10-12 21:27:55 -0500

    paralela

---
+++ src/paralela/SimpleThread.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paralela;
+
+/**
+ *
+ * @author personal
+ */
+public class SimpleThread extends Thread {
+    public void run(){
+        System.out.println("soy un hilo");
+    }
+    public static void main(String[] args) {
+        SimpleThread s = new SimpleThread();
+        s.setDaemon(true);
+        s.start();
+    }
+}
